b95a167d3651b316e0f770861ffcc565abec7294
Bijay Jena <bijay.jena@squbix.com>
2022-11-28 09:10:04 +0530

    saved progress

--- app/src/main/java/com/squbix/wifi2/WiFiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/squbix/wifi2/WiFiDirectBroadcastReceiver.kt
@@ -20,25 +20,31 @@ class WiFiDirectBroadcastReceiver(
         when (intent.action) {
             WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
                 // Check to see if Wi-Fi is enabled and notify appropriate activity
-                if (intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE,0) == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                    Toast.makeText(context, "WiFi is ON!", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(context, "WiFi is OFF!", Toast.LENGTH_SHORT).show()
+                when (intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        Toast.makeText(context, "WiFi is ON!", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        Toast.makeText(context, "WiFi is OFF!", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
                 Log.d("WiFiDirectBR","WifiP2pManager.requestPeers() invoked")
-                manager.requestPeers(channel,activity.peerListListener)
+//                manager.requestPeers(channel,activity.peerListListener)
             }
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
                 // Respond to new connection or disconnections
                 Log.d("WiFiDirectBR","Connection changed.")
                 val networkInfo: NetworkInfo? = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO)
                 if (networkInfo!!.isConnected) {
-                    manager.requestConnectionInfo(channel, activity.connectionInfoListener)
+                    activity.connState.text = context.getString(R.string.dvc_con)
+                    Log.i("WiFiDirectBR","Device Connected")
+//                    manager.requestConnectionInfo(channel, activity.connectionInfoListener)
                 } else {
                     activity.connState.text = context.getString(R.string.dvc_discon)
+                    Log.i("WiFiDirectBR","Device Disconnected")
                 }
             }
             WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
